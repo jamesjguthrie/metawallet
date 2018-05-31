4fd7da0d42bab4ca31f4de7da9eaaa0a90baa21d
Daniel Stewart <daniel@paradimeapp.com>
2018-05-30 20:20:23 -0500

    Move Placeholder Panel to Left

--- metawallet/app/view/tablet/Balances.js
+++ metawallet/app/view/tablet/Balances.js
@@ -10,29 +10,12 @@ Ext.define('FW.view.tablet.Balances', {
     config: {
         layout: 'hbox',
         items: [{
-            xtype: 'fw-balanceslist',
-            width: 320
-        },
-        {
-            xtype: 'fw-ethbalanceslist',
-            width: 320
-        },
-        {
-            xtype: 'fw-erc20tokenslist',
-            width: 320
-        },
-        {
-            xtype: 'fw-ltcbalanceslist',
-            width: 320
-        },
-        {
             flex: 1,
             xtype: 'container',
             layout: 'card',
             itemId: 'balances',
             cls: 'fw-panel fw-panel-separator',
             items: [{
-                // Define placeholder to display before we have any currency selected
                 xtype: 'container',
                 itemId: 'placeholder',
                 scrollable: 'vertical',
@@ -41,21 +24,33 @@ Ext.define('FW.view.tablet.Balances', {
                     align: 'center',
                     pack: 'top'
                 },
-                items:[{
+                items: [{
                     xtype: 'fw-toptoolbar',
                     menu: true
-                },{
+                }, {
                     margin: '10 0 0 0',
-                    html:'<center><img src="resources/images/logo.png" width="90%" style="max-width:350px;"></center>'
-                },{
-                    margin: '10 0 0 0',
-                    cls: 'fw-currencyinfo-instructions',
-                    html:'<center>Please select an item<br/>from the list on the left</center>'
+                    html: '<center><img src="resources/images/logo.png" width="90%" style="max-width:350px;"></center>'
                 }]
-            },{
+            }, {
                 xtype: 'fw-tokeninfo',
                 flex: 1
             }]
+        },
+        {
+            xtype: 'fw-balanceslist',
+            flex: 1
+        },
+        {
+            xtype: 'fw-ethbalanceslist',
+            flex: 1
+        },
+        {
+            xtype: 'fw-erc20tokenslist',
+            flex: 1
+        },
+        {
+            xtype: 'fw-ltcbalanceslist',
+            flex: 1
         }]
     }
 
